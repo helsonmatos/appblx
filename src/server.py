3991cdecde0018fe11088fbea5ac916a7ff3edb9
Helson Matos <helson.11@hotmail.com>
2023-09-01 23:22:50 -0300

    db session

--- src/server.py
+++ src/server.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from src.schemas.schemas import Produto
+from src.infra.sqlalchemy.config.database import get_db
+from src.infra.sqlalchemy.repositorios.produto import RepositorioProduto
 
 app = FastAPI()
 
 @app.post('/produto')
-def criar_produto(produto: Produto):
+def criar_produto(produto: Produto, db: Session = Depends(get_db())):
+    produto_criado = RepositorioProduto().criar(produto)
     return {'Msg': 'Criando'}
 
 
